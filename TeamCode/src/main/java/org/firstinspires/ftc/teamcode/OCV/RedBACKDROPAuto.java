cbe2261acd3fae3348b67058abeef58738a66023
cnstewrt <chad@candoreng.com>
2024-02-25 19:33:59 -0600

    Trajectories testing in Meep Meep

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OCV/RedBACKDROPAuto.java
@@ -0,0 +1,156 @@
+package org.firstinspires.ftc.teamcode.OCV;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+@Autonomous
+public class RedBACKDROPAuto extends OpMode {
+    private OCVVisionProc drawProcessor;
+    private VisionPortal visionPortal;
+    private Servo pixelDrop = null;
+    private Servo backPixelDrop = null;
+    int positionDetect = 0;
+
+    @Override
+    public void init() {
+        drawProcessor = new OCVVisionProc();
+        pixelDrop = hardwareMap.get(Servo.class, "pDrop");
+        backPixelDrop = hardwareMap.get(Servo.class, "puDrop");
+        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), drawProcessor);
+        visionPortal.resumeStreaming();
+    }
+
+    @Override
+    public void init_loop() {
+        visionPortal.resumeStreaming();
+        visionPortal.setProcessorEnabled(drawProcessor, true);
+
+        switch (drawProcessor.getSelection()) {
+            case LEFT:
+                positionDetect = 1;
+                break;
+            case MIDDLE:
+                positionDetect = 2;
+                break;
+            case RIGHT:
+                positionDetect = 3;
+                break;
+            case NONE:
+                positionDetect = 0;
+                break;
+        }
+    }
+
+    @Override
+    public void start() {
+
+        visionPortal.resumeStreaming();
+        visionPortal.setProcessorEnabled(drawProcessor, true);
+    }
+
+    @Override
+    public void loop()  {
+
+        telemetry.addData("Identified", drawProcessor.getSelection());
+
+
+        switch (positionDetect) {
+            case 1:
+                visionPortal.setProcessorEnabled(drawProcessor, false);
+                visionPortal.stopStreaming();
+                SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+                Pose2d startPoseL = new Pose2d(11, -67, Math.toRadians(-90));
+
+                drive.setPoseEstimate(startPoseL);
+
+                TrajectorySequence trajL = drive.trajectorySequenceBuilder(startPoseL)
+                        .lineToConstantHeading(new Vector2d(16, -41))
+
+                        .addDisplacementMarker(() -> {
+                            pixelDrop.setPosition(0);
+                        })
+
+                        .lineToLinearHeading(new Pose2d(13.5, -40, Math.toRadians(-35)))
+                        .lineToLinearHeading(new Pose2d(40, -25, Math.toRadians(180)))
+                        .lineToConstantHeading(new Vector2d(51, -25))
+
+                        .addDisplacementMarker(() -> {
+                            backPixelDrop.setPosition(0);
+                        })
+
+                        .build();
+
+                drive.followTrajectorySequence(trajL);
+
+                visionPortal.setProcessorEnabled(drawProcessor, false);
+                positionDetect = 0;
+                break;
+            case 3:
+                visionPortal.setProcessorEnabled(drawProcessor, false);
+                visionPortal.stopStreaming();
+                drive = new SampleMecanumDrive(hardwareMap);
+
+                Pose2d startPoseR = new Pose2d(11, -67, Math.toRadians(-90));
+
+                drive.setPoseEstimate(startPoseR);
+                TrajectorySequence trajR = drive.trajectorySequenceBuilder(startPoseR)
+                        .lineToConstantHeading(new Vector2d(27.00, -60.00))
+                        .lineToConstantHeading(new Vector2d(27.00, -42.00))
+
+                        .addDisplacementMarker(() -> {
+                            pixelDrop.setPosition(0);
+                        })
+
+                        .lineToConstantHeading(new Vector2d(27.00, -45.00))
+                        .lineToLinearHeading(new Pose2d(40, -38.75, Math.toRadians(180)))
+                        .lineToConstantHeading(new Vector2d(51.00, -38.75))
+
+                        .addDisplacementMarker(() -> {
+                            backPixelDrop.setPosition(0);
+                        })
+                        .build();
+                drive.followTrajectorySequence(trajR);
+                visionPortal.setProcessorEnabled(drawProcessor, false);
+                positionDetect = 0;
+                break;
+            case 2:
+                visionPortal.setProcessorEnabled(drawProcessor, false);
+                visionPortal.stopStreaming();
+                drive = new SampleMecanumDrive(hardwareMap);
+
+                Pose2d startPoseM = new Pose2d(11, -67, Math.toRadians(-90));
+
+                drive.setPoseEstimate(startPoseM);
+                TrajectorySequence trajM = drive.trajectorySequenceBuilder(startPoseM)
+                        .lineToConstantHeading(new Vector2d(12.00, -42.00))
+                        .lineToConstantHeading(new Vector2d(12.00, -48.5))
+
+                        .addDisplacementMarker(() -> {
+                            pixelDrop.setPosition(0);
+                        })
+
+                        .lineToLinearHeading(new Pose2d(40, -31.5, Math.toRadians(180)))
+                        .lineToConstantHeading(new Vector2d(51.00, -31.5))
+
+                        .addDisplacementMarker(() -> {
+                            backPixelDrop.setPosition(0);
+                        })
+                        .build();
+                drive.followTrajectorySequence(trajM);
+                visionPortal.setProcessorEnabled(drawProcessor, false);
+                positionDetect = 0;
+                break;
+            case 0:
+                break;
+        }
+    }
+}
