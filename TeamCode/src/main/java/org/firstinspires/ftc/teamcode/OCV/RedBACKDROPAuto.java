d512551304e102b829717e649d77e276f9a855a9
yasha.burns <yasha@wds4u.com>
2024-04-29 08:36:40 -0500

    final everything. no longer touching code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OCV/RedBACKDROPAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OCV/RedBACKDROPAuto.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -70,21 +71,23 @@ public class RedBACKDROPAuto extends OpMode {
                 Pose2d startPoseL = new Pose2d(11, -61, Math.toRadians(-90));
                 drive.setPoseEstimate(startPoseL);
                 TrajectorySequence trajL = drive.trajectorySequenceBuilder(startPoseL)
-                        .lineToConstantHeading(new Vector2d(11, -40))
-                        .lineToLinearHeading(new Pose2d(10, -35, Math.toRadians(-35)))
-                        .lineToConstantHeading(new Vector2d(13, -35))
+                        .lineToConstantHeading(new Vector2d(11, -41))
+                        .lineToLinearHeading(new Pose2d(10, -36, Math.toRadians(-30)))
+                        .lineToConstantHeading(new Vector2d(13, -36))
 
-                        .addDisplacementMarker(15.75,() -> {
+                        .addDisplacementMarker(16,() -> {
                             pixelDrop.setPosition(0);
                         })
 
-                        .lineToLinearHeading(new Pose2d(20, -33, Math.toRadians(-90)))
+                        .lineToLinearHeading(new Pose2d(40, -23, Math.toRadians(180)),
+                                SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
+)
 
                         .addDisplacementMarker(() -> {
                             pixelDrop.setPosition(1);
                         })
 
-                        .lineToLinearHeading(new Pose2d(40, -23, Math.toRadians(180)))
                         .lineToConstantHeading(new Vector2d(50, -24.5))
 
                         .addDisplacementMarker(() -> {
@@ -92,13 +95,17 @@ public class RedBACKDROPAuto extends OpMode {
                         })
 
                         .lineToConstantHeading(new Vector2d(51, -24.5))
-                        .lineToConstantHeading(new Vector2d(45, -24.5))
+                        .lineToConstantHeading(new Vector2d(45, -24.5),
+                                SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
 
                         .addDisplacementMarker(() -> {
                             backPixelDrop.setPosition(1);
                         })
 
-                        .lineToConstantHeading(new Vector2d(45, -60))
+                        .lineToConstantHeading(new Vector2d(45, -60),
+                                SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
                         .lineToConstantHeading(new Vector2d(60,-60))
 
                         .build();
@@ -131,21 +138,27 @@ public class RedBACKDROPAuto extends OpMode {
                         })
 
                         .lineToLinearHeading(new Pose2d(38, -40, Math.toRadians(180)))
-                        .lineToConstantHeading(new Vector2d(48.5, -37))
+                        .lineToConstantHeading(new Vector2d(50, -36))
 
                         .addDisplacementMarker(() -> {
                             backPixelDrop.setPosition(0.78);
                         })
 
-                        .lineToConstantHeading(new Vector2d(50, -37))
-                        .lineToConstantHeading(new Vector2d(44, -37))
+                        .lineToConstantHeading(new Vector2d(51, -36))
+                        .lineToConstantHeading(new Vector2d(44, -36),
+                                SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
 
                         .addDisplacementMarker(() -> {
                             backPixelDrop.setPosition(1);
                         })
 
-                        .lineToConstantHeading(new Vector2d(44, -60))
-                        .lineToConstantHeading(new Vector2d(60, -60))
+                        .lineToConstantHeading(new Vector2d(44, -60),
+                                SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                        .lineToConstantHeading(new Vector2d(60, -60),
+                                SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
                         .build();
                 drive.followTrajectorySequence(trajR);
                 visionPortal.setProcessorEnabled(drawProcessor, false);
@@ -173,22 +186,32 @@ public class RedBACKDROPAuto extends OpMode {
                         .addDisplacementMarker(() -> {
                             pixelDrop.setPosition(1);
                         })
-                        .lineToConstantHeading(new Vector2d(49.0, -30))
+                        .lineToConstantHeading(new Vector2d(50.0, -30),
+                                SampleMecanumDrive.getVelocityConstraint(40, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
 
                         .addDisplacementMarker(() -> {
                             backPixelDrop.setPosition(0.78);
                         })
 
-                        .lineToConstantHeading(new Vector2d(50.00, -28))
-                        .lineToConstantHeading(new Vector2d(49, -28))
+                        .lineToConstantHeading(new Vector2d(51.00, -28))
+                        .lineToConstantHeading(new Vector2d(49, -28),
+                                SampleMecanumDrive.getVelocityConstraint(40, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
 
                         .addDisplacementMarker(() -> {
                             backPixelDrop.setPosition(1);
                         })
 
-                        .lineToConstantHeading(new Vector2d(45, -30))
-                        .lineToConstantHeading(new Vector2d(45, -60))
-                        .lineToConstantHeading(new Vector2d(59, -59))
+                        .lineToConstantHeading(new Vector2d(45, -30),
+                                SampleMecanumDrive.getVelocityConstraint(40, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                        .lineToConstantHeading(new Vector2d(45, -60),
+                                SampleMecanumDrive.getVelocityConstraint(40, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                        .lineToConstantHeading(new Vector2d(59, -59),
+                                SampleMecanumDrive.getVelocityConstraint(40, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
                         .build();
                 drive.followTrajectorySequence(trajM);
                 visionPortal.setProcessorEnabled(drawProcessor, false);
